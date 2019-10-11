98b297e1b95553e4de1382e3cd80f2ce9b5ac54b
Daniel <coronaortdan@hotmail.com>
2019-10-10 19:06:47 -0500

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MapComponent } from './map/map.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MapComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AgmCoreModule.forRoot({ 
+      apiKey: 'AIzaSyCo5eHkeqhurDSk1LwGLQk0D2YUI8pbKhM'// Please use your own key here !!
+    })
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnChanges ,OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent implements OnInit,OnChanges  {
+  
+  city: string;
+  temp: number;
+
+  @Input() latitude: string;
+  @Input() longitude: string;
+   
+  
+  
+
+  constructor (private http: HttpClient) { }
+
+    
+  ngOnInit() {
+    
+  }
+
+  ngOnChanges(){
+
+    let url = 'http://api.openweathermap.org/data/2.5/weather?'
+    + 'lat=' + this.latitude
+    + '&lon=' + this.longitude
+    + '&units=metric'
+    + '&lang=en'
+    + '&appid=6a145f2420c05499d548551c124b3ce7'; // your api key here
+  
+    this.http.get(url).subscribe((data: any) => {
+      console.log(data);
+      this.city = data.name;
+      this.temp = data.main.temp.toFixed(1);
+    });
+  }
+}
