240ff7755c8b32078fb3bec60d9120d39c2012b7
Daniel <corona_ort@hotmail.com>
2019-10-13 06:51:43 -0500

    modify components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import {FormsModule} from '@angular/forms';
+//import {HttpModule} from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { MapComponent } from './map/map.component';
 import { WeatherComponent } from './weather/weather.component';
+
 
 
 @NgModule({
@@ -20,6 +22,8 @@ import { WeatherComponent } from './weather/weather.component';
     ],
   imports: [
     BrowserModule,
+    FormsModule,
+   // HttpModule,
     HttpClientModule,
     AgmCoreModule.forRoot({ 
       apiKey: 'AIzaSyCo5eHkeqhurDSk1LwGLQk0D2YUI8pbKhM'// Please use your own key here !!
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,24 +1,13 @@
 //import { Component, OnInit } from '@angular/core';
 import { MouseEvent } from '@agm/core';
 import { Component,OnInit} from '@angular/core';
-import {trigger, state, style, animate, transition } from '@angular/animations';
+//import {trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
-  animations:[
-    trigger('contenedorAnimable',[
-      state('initial', style({
-        opacity:0
-      })),
-      state('final', style({
-        opacity:1
-      })),
-      transition('initial => final',animate(2000)),
-      transition('final => initial',animate(1000))
-    ])
-  ]
+  
 })
 export class MapComponent implements OnInit {
 
@@ -32,19 +21,7 @@ export class MapComponent implements OnInit {
    
   }
 
-  labelOptions = {
-    color: '#ee4646',
-    fontFamily: '',
-    fontSize: '10px',
-    fontWeight: 'bold',
-    letterSpacing: '0.5px',
-    text: 'Weather-map'
-  }
-
-  iconMap = {
-    iconUrl:"https://1.bp.blogspot.com/-PxgBgllxJrE/VpgleojkmyI/AAAAAAAAEAs/NOibbG2gPUc/s1600/google-marker-preview1-177x300.png",
-    iconHeigh:10
-  }
+ 
 
   constructor() { }
 
